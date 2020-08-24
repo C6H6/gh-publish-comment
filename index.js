b43898908c9fffb22ae957b78996b222ecbb30d7
Patryk Wieczorek <c3c3h3h3@gmail.com>
2020-08-24 17:55:05 +0200

    Init

---
+++ index.js
@@ -0,0 +1,51 @@
+import { getInput, setFailed } from '@actions/core';
+import { context as _context, getOctokit } from '@actions/github';
+
+
+async function run() {
+    try {
+        const token = getInput('GITHUB_TOKEN');
+        const taskNumber = getInput('task_number');
+        const urlPrefix = getInput('url_prefix');
+        const url = String(urlPrefix) + String(taskNumber)
+
+        const pr_number = _context.payload.pull_request.number;
+
+        const octokit = getOctokit(token);
+
+        const repository = process.env.GITHUB_REPOSITORY;
+        const [owner, repo] = repository.split("/");
+
+        const { data: comments } = await octokit.issues.listComments({
+            owner: owner,
+            repo: repo,
+            issue_number: pr_number,
+        });
+
+        const botLogin = 'github-actions[bot]'
+
+        const comment = comments.find(c => {
+            return c.user.login == botLogin
+        })
+
+        if (comment) {
+            octokit.issues.updateComment({
+                ..._context.repo,
+                comment_id: comment.id,
+                issue_number: pr_number,
+                body: url
+            })
+        } else {
+            octokit.issues.createComment({
+                ..._context.repo,
+                issue_number: pr_number,
+                body: url
+            })
+        }
+
+    } catch (error) {
+        setFailed(error.message);
+    }
+}
+
+run()
